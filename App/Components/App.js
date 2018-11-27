d26e9b41cced0715f05d3c10bc87f0becfc5e609
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-26 20:25:13 -0600

    Start new project, add webpack and run hello world

---
+++ App/Components/App.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactRouter = require('react-router-dom');
+
+
+
+class App extends React.Component{
+	render(){
+		return(
+			<div>
+			<h1>Hello World</h1>
+			</div>
+
+		)
+	}
+}
+
+module.exports = App;
