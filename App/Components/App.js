359ff4298abeb5ccda4e43dc04ee5eb8cb94d26b
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-27 21:08:53 -0600

    Add Style

--- App/Components/App.js
+++ App/Components/App.js
@@ -7,7 +7,34 @@ class App extends React.Component{
 	render(){
 		return(
 			<div>
-			<h1>Hello World</h1>
+			  <div>
+    <div className="container">
+        
+            <div className="alert alert-danger" role="alert">
+          
+            </div>
+        
+            <div className="alert alert-success" role="alert">
+                
+            </div>
+       
+    </div>
+    
+    <div id="landing-header">
+ 		<h1>Fuel Your Body!</h1>
+		<a href="/campgrounds" className="btn btn-lg btn-success">Buy Now!</a>
+    </div>
+    
+    <ul className="slideshow">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div>
+			</div>
+			</div>
 			</div>
 
 		)
@@ -15,3 +42,7 @@ class App extends React.Component{
 }
 
 module.exports = App;
+
+
+      
+ 
